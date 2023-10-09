424674e80418b302b71a65c566355a70cd906035
BA46466 <ba46466@zut.edu.pl>
2023-10-09 23:27:03 +0200

    #2

--- labA/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-$duck = new \Abartman\LabComposer\Duck();
-$duck->quack();
-$duck->quack();
-$duck->quack();
--- labA/src/Duck.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Abartman\LabComposer;
-class Duck
-{
-    public function quack()
-    {
-        echo "Quack\n";
-    }
-}
